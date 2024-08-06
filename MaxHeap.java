1e7111c06557701cc2be92f28a5c58243d1b1bba
xie <xiewenchao1999@163.com>
2024-08-06 11:30:47 +0800

    Top-k 问题：基于堆查找数组中最大的 k 个元素

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MaxHeap {
     List<Integer> maxHeap = new ArrayList<>();
@@ -117,5 +119,31 @@ public class MaxHeap {
         for (int i = parent(size() - 1); i >= 0; i--) {
             siftDown(i);
         }
+    }
+    
+    /**
+     * 基于堆查找数组中最大的 k 个元素
+     * 
+     * 时间复杂度：O(nlogk)
+     * @param nums  数组
+     * @param k 最大元素个数
+     * @return  包含最大的 k 个元素的堆
+     */
+    Queue<Integer> topKHeap(int[] nums, int k) {
+        // 初始化小顶堆
+        Queue<Integer> heap = new PriorityQueue<>();
+        // 将数组的前 k 个元素入堆
+        for (int i = 0; i < k; i++) {
+            heap.offer(nums[i]);
+        }
+        // 从第 k + 1 个元素开始，保持堆的长度为 k
+        for (int i = k; i < nums.length; i++) {
+            // 若当前元素大于堆顶元素，则将堆顶元素出堆、当前元素入堆
+            if (nums[i] > heap.peek()) {
+                heap.poll();
+                heap.offer(nums[i]);
+            }
+        }
+        return heap;
     }
 }
